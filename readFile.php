8571530ddb7c49b1f3ff8b033e80b1884f81758d
RubyCouz <ced270784@gmail.com>
2021-03-12 09:08:43 +0100

    nettoyage code

---
+++ readFile.php
@@ -0,0 +1,28 @@
+<?php
+// définition const contenant chemin vers racine du projet
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+// ouverture du fichier et stockage sous forme de tableau
+$fileReader = file(ROOT . '/dico/list.txt');
+// définition d'un tableau et d'une variable de comptage d'erreur
+$dico = [];
+$count = 0;
+// parcours du tableau (fichier) et stockage dans un tableau sous utf8
+foreach ($fileReader as $key => $value) {
+    array_push($dico, trim(utf8_encode($value)));
+}
+// si une donnée est envoyé
+if (isset($_POST['value'])) {
+    // définitions d'un tableau d'erreur
+//    $error = [];
+    // séparation de la valeur récupérer et stockage dans un tableau
+    $valueArray = explode(' ', $_POST['value']);
+    // par cour du tableau contenant la valuer envoyé
+    foreach ($valueArray as $key => $value) {
+        // si la valeur saisie n'est pas dans le dico
+        if (!in_array($value, $dico)) {
+           $count++;
+//            array_push($error, $value);
+        }
+    }
+    echo $count;
+}
