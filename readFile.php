b0a9c8f7ea107516f2c2d3e7bb82afbac0cc4c38
RubyCouz <ced270784@gmail.com>
2021-03-18 13:09:42 +0100

    ajout filtre

--- readFile.php
+++ readFile.php
@@ -6,12 +6,15 @@ $fileReader = file(ROOT . '/dico/list.txt');
 // définition d'un tableau et d'une variable de comptage d'erreur
 $dico = [];
 $count = 0;
+$regexTag = '/^[\@][a-zA-Z]+[\#][\d]{4}$/';
+$regexBot = '/^[\!][a-z]+$/';
 // parcours du tableau (fichier) et stockage dans un tableau sous utf8
 foreach ($fileReader as $key => $value) {
     array_push($dico, trim(utf8_encode($value)));
 }
 // si une donnée est envoyé
 if (isset($_POST['value'])) {
+
     // définitions d'un tableau d'erreur
 //    $error = [];
     // séparation de la valeur récupérer et stockage dans un tableau
@@ -19,10 +22,13 @@ if (isset($_POST['value'])) {
     // par cour du tableau contenant la valuer envoyé
     foreach ($valueArray as $key => $value) {
         // si la valeur saisie n'est pas dans le dico
-        if (!in_array($value, $dico)) {
-           $count++;
+        if(!preg_match($regexTag, $value) && filter_var($value, FILTER_VALIDATE_URL) === false && !preg_match($regexBot, $value)) {
+            if (!in_array($value, $dico)) {
+                $count++;
 //            array_push($error, $value);
+            }
         }
+
     }
     echo $count;
 }
